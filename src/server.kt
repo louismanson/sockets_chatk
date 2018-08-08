6955ee757e6c446bd73f9192577a959b32f55ab8
Louis <manson.louis@gmail.com>
2018-08-08 10:19:37 -0700

    create funtions for create/close conextion, send data and int sockets

--- src/server.kt
+++ src/server.kt
@@ -1,10 +1,55 @@
 import java.io.BufferedReader
+import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.io.InputStreamReader
 import java.net.ServerSocket
 import java.net.Socket
 
 
 private var socket: Socket? = null
+private var inBuffer: DataInputStream? = null
+private var outBuffer: DataOutputStream? = null
+
+fun createConextion(ip: String, port: Int) {
+    try {
+        socket = Socket(ip,port)
+        println("Server Started and listening to the port 25000")
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+}
+
+fun dataFlow(){
+    try {
+        inBuffer = DataInputStream(socket?.getInputStream())
+        outBuffer = DataOutputStream(socket?.getOutputStream())
+        outBuffer?.flush()
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+}
+
+fun sendData(message: String){
+    try {
+        outBuffer?.writeUTF(message)
+        outBuffer?.flush()
+    }catch (e: Exception){
+
+    }
+}
+
+fun closeConextion(){
+    try {
+        outBuffer?.close()
+        inBuffer?.close()
+        socket?.close()
+    }catch (e: Exception){
+        e.printStackTrace()
+    }finally {
+        System.exit(0)
+    }
+
+}
 
 fun main(args: Array<String>){
     try {
