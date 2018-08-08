b611258bfb97956c94c575911511888f749af266
Louis <manson.louis@gmail.com>
2018-08-08 15:29:11 -0700

    add client functions

--- src/client.kt
+++ src/client.kt
@@ -1,11 +1,95 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.*
 import java.net.InetAddress
 import java.net.Socket
+import java.util.*
 
 private var socket: Socket? = null
+private var inBuffer: DataInputStream? = null
+private var outBuffer: DataOutputStream? = null
+internal var teclado = Scanner(System.`in`)
+
+
+private fun createConnection(ip: String, port: Int) {
+    try {
+        socket = Socket(ip,port)
+        println("Server Started and listening to the port 25000")
+    }catch (e: Exception){
+        e.printStackTrace()
+    }
+}
+
+private fun dataFlow(){
+    try {
+        inBuffer = DataInputStream(socket?.getInputStream())
+        outBuffer = DataOutputStream(socket?.getOutputStream())
+        outBuffer?.flush()
+    }catch (e: IOException){
+        e.printStackTrace()
+    }
+}
+
+private fun sendData(message: String){
+    try {
+        outBuffer?.writeUTF(message)
+        outBuffer?.flush()
+    }catch (e: IOException){
+
+    }
+}
+
+private fun reciveData(){
+    var st = ""
+    try {
+        while (true){
+            st = inBuffer!!.readUTF()
+            println("Client: $st")
+            print("You: ")
+        }
+    }catch (e: IOException){
+        closeConnection()
+    }
+}
+
+private fun writeData90{
+    var input: String
+    while (true){
+        println("You: ")
+        input = teclado.nextLine()
+        if (input.isNotEmpty()){
+            sendData(input)
+        }
+    }
+}
+
+
+
+private fun closeConnection(){
+    try {
+        outBuffer?.close()
+        inBuffer?.close()
+        socket?.close()
+        println("end")
+    }catch (e: IOException){
+        e.printStackTrace()
+    }finally {
+        System.exit(0)
+    }
+}
+
+
+fun executeConnection(ip: String, port: Int){
+    val thread = Thread(Runnable {
+        try {
+            createConnection(ip,port)
+            dataFlow()
+
+        }finally {
+            closeConnection()
+        }
+    })
+    thread.start()
+
+}
 
 fun main (arg: Array<String>){
     while (true){
